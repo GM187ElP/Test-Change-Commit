0cc521ef7cb19890109f59c340d987c111b5996a
unknown <arsalan.akorn@gmail.com>
2024-06-15 21:54:25 +0330

    say hello develop

--- changeGitCommit/Program.cs
+++ changeGitCommit/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 WrithMessage();
+
+WrithDevelop();
 static void WrithMessage()
 {
     Console.WriteLine("hello world");
+}
+
+
+static void WrithDevelop()
+{
+    Console.WriteLine("say hlloe develop");
 }
